59db1f1265859e03429e6d60a4b59bf70e9261c5
Diego <d.rincon@utp.edu.co>
2024-05-27 21:52:33 -0500

    Back

--- server/src/app.js
+++ server/src/app.js
@@ -12,14 +12,16 @@ app.use(morgan("dev")); //para ver las peticiones que llegan al servidor
 app.use(express.json()); //para que el servidor entienda los json que llegan al servidor
 app.use(cookieParser()); //para que el servidor entienda las cookies que llegan al servidor
 
-app.use("/api/booking", bookingRoutes)
-app.use("/api/auth", authRoutes)
-
 app.use(cors({
-  origin: URL_FRONTEND
+  origin: URL_FRONTEND,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // This allows cookies to be sent
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 
-
+app.use("/api/booking", bookingRoutes)
+app.use("/api/auth", authRoutes)
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Endpoint not found" });
